d2681a054e814f4bb19ae394ca6561013030ad4c
soyaliyamansi-2007 <soyaliyamansi@gmail.com>
2026-08-19 09:42:03 +0530

    Add program to sum two numbers from user input

---
+++ AddTwoNumbers.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        int sum = num1 + num2;
+
+        Console.WriteLine("Sum = " + sum);
+    }
+}
